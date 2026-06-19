4c8e535e611198459d53804b982346fdca7a5ee4
Minhazul Hayat Khan <minhaz1217@gmail.com>
2026-06-19 23:26:42 +0600

    detection working based on bounds

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var play_zone: ColorRect = $PlayZone
 @onready var player: StaticBody2D = $Player
 @onready var catch_zone: Area2D = $CatchZone
-@onready var catch_zone_rect: ColorRect = $CatchZone/CatchZoneRect
 @onready var player_rect: ColorRect = $Player/PlayerRect
 
 
@@ -13,23 +12,24 @@ var catch_zone_size = 50
 var player_size = 50
 
 func _ready() -> void:
-	var playerRect = player.get_node("PlayerRect").size
+	player_size = player.get_node("PlayerRect").size
+	catch_zone_size = catch_zone.get_node("CatchZoneRect").size
+	
+	print(player_size)
 	
 func _process(delta: float) -> void:
 	var player_speed := speed * delta
-	if(player.position.x + player.size.x > play_zone.position.x + play_zone.size.x 
+	if(player.position.x + player_size.x > play_zone.position.x + play_zone.size.x
 		|| player.position.x < play_zone.position.x ):
 		player_direction *= -1
 	player.position.x += player_speed * player_direction
-
-
-func _on_catch_zone_area_body_entered(body: Node2D) -> void:
-	print("Something Entered ", body)
-
-
-func _on_catch_zone_area_area_entered(area: Area2D) -> void:
-	print("Something Entered ", area)
-
-
-func _on_catch_zone_area_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print("Something Entered 3", body)
+	
+	
+	#if(player.global_position.x + player_size.x > play_zone.global_position.x + play_zone.size.x
+		#|| player.global_position.x < play_zone.global_position.x ):
+		#player_direction *= -1
+	#player.global_position.x += player_speed * player_direction
+	
+	if(Input.is_key_pressed(KEY_SPACE)):
+		if( player.position.x >= catch_zone.position.x && player.position.x <= catch_zone.position.x + catch_zone_size.x):
+			print("In Range")
