769b8e9e472a45f496ed7ea8badf1454b4bf7da9
Minhazul Hayat Khan <minhaz1217@gmail.com>
2026-06-20 04:27:56 +0600

    godot 4.6.3 backup

--- scenes/game.gd
+++ scenes/game.gd
@@ -6,20 +6,22 @@ extends Node2D
 @export var player_rect: Sprite2D
 @export var player_spawn_point : ColorRect
 @export var player_image = preload("uid://61nv8tui4ft2")
-
+@export var score_label: Label
 
 var speed : int = 200
 var player_direction = 1
-var catch_zone_size = 50
 var player_size = 50
+var score = 0
+var paused = true
 
 func _ready() -> void:
-	#player_size = player.get_node("PlayerRect").size
-	#catch_zone_size = catch_zone.get_node("CatchZoneRect").size
-	print(play_zone.size.x)
 	spawn_player()
 	
 func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("primary_action")):
+		start_game()
+		
+	if(paused): return
 	var player_speed := speed * delta
 	
 	var player_size_x = (Vector2(32 , 32) * player.scale.x).x
@@ -34,7 +36,6 @@ func _process(delta: float) -> void:
 		spawn_player()
 	player.position.x += player_speed * player_direction
 	
-	
 	#if(player.global_position.x + player_size.x > play_zone.global_position.x + play_zone.size.x
 		#|| player.global_position.x < play_zone.global_position.x ):
 		#player_direction *= -1
@@ -42,14 +43,60 @@ func _process(delta: float) -> void:
 	handle_input()
 		#if( player.position.x >= catch_zone.position.x && player.position.x <= catch_zone.position.x + catch_zone_size.x):
 			#print("In Range")
-
+func start_game():
+	score = 0
+	spawn_player()
+	paused = false
+	$Helper.text = ""
+	
+func game_over():
+	if(score >= 100):
+		$Helper.text = "YEY you've reached finish line.\nHope you enjoyed it"
+		paused = true
+	
 func handle_input():
+	if(Input.is_action_just_pressed("ui_down")):
+		if(is_in_zone()):
+			if(player.rotation_degrees == 0.0):
+				increase_score()
+			else:
+				wrong_pressed()
+	
+	if(Input.is_action_just_pressed("ui_left")):
+		if(is_in_zone()):
+			if(player.rotation_degrees == 90.0):
+				increase_score()
+			else:
+				wrong_pressed()
+	
+	if(Input.is_action_just_pressed("ui_up")):
+		if(is_in_zone()):
+			if(player.rotation_degrees == 180.0):
+				increase_score()
+			else:
+				wrong_pressed()
+	
+	if(Input.is_action_just_pressed("ui_right")):
+		if(is_in_zone()):
+			if(player.rotation_degrees == 270.0):
+				increase_score()
+			else:
+				wrong_pressed()
+			
 	if(Input.is_action_just_pressed("primary_action")):
 		is_in_zone()
 	
 	if(Input.is_action_just_pressed("test_action")):
 		spawn_player()
 
+func wrong_pressed():
+	var helper_text = [ "Are you sure you pressed the right button? 🙄", "The name is 'DO NO RIGHT', not 'DO THE OBVIOUS'" ]
+	$Helper.text = helper_text.pick_random()
+func increase_score():
+	score+= 10
+	score_label.text = "Score: " + str(score)
+	spawn_player()
+	
 func spawn_player():
 	var temp_player: Sprite2D = Sprite2D.new()
 	temp_player.texture = player_image
@@ -62,4 +109,11 @@ func spawn_player():
 	add_child(temp_player)
 
 func is_in_zone():
-	pass
+	#print("Checking")
+	var catch_zone_size = 128 * catch_zone.scale.x
+	var player_size_x = (Vector2(32 , 32) * player.scale.x).x
+	var player_position = player.position.x - (player_size_x /2)
+	if(player_position >= catch_zone.position.x && player_position <= catch_zone.position.x + catch_zone_size):
+		print(player.rotation_degrees)
+		return true
+	return false
